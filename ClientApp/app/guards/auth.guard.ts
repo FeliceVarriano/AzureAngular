4f97065ef230a821e1dd45dac3ab83a841c92a64
Felice Varriano <felice.varriano@gmail.com>
2022-08-10 15:26:11 -0400

    following tutorial - testing CI

---
+++ ClientApp/app/guards/auth.guard.ts
@@ -0,0 +1,18 @@
+﻿import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router'
+import { AuthService } from '../services/auth.service';
+import { AuthModule } from '@auth0/auth0-angular';
+import { Router } from '@angular/router';
+
+//@Injectable()
+//export class AuthGuard implements CanActivate {
+//    constructor(private authService: AuthService, private router: Router) { }
+//    canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+//        if (true this.authService.isAuthenticated()) {
+//            return true; //valid
+//        } else {
+//            this.router.navigate(['/home']);
+//            return false; //invalid
+//        }
+//    }
+//}
---
+++ ClientApp/app/services/auth.service.ts
@@ -0,0 +1,22 @@
+﻿import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from '../components/app/app.component';
+
+// Import the module from the SDK
+import { AuthModule } from '@auth0/auth0-angular';
+
+@NgModule({
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+
+        // Import the module into the application, with configuration
+        AuthModule.forRoot({
+            domain: 'angular-on-azure.us.auth0.com',
+            clientId: 'c2jBAzj13wudsiaMjy3AR8NtKfTv0IYX'
+        }),
+    ],
+
+    bootstrap: [AppComponent]
+})
+export class AuthService  { }
